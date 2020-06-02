62cb439db04214d181ed4d44e823226a1ce97149
jack-mil <>
2020-06-02 08:44:47 -0400

    added --dry-run option; some edge cases

--- bing_search.py
+++ bing_search.py
@@ -7,25 +7,18 @@ import time
 import webbrowser
 from urllib.parse import quote_plus
 import pyautogui
-import requests
+# import requests
 
-# header = {
-#   "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36 Edg/83.0.478.37"
-# }
-# header_mobile = {
-#   "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
-# }
-
-count = 30
+DEFUALT_COUNT = 30
 sleep_time = 2
 
 url = 'https://www.bing.com/search?q='
 
-# word_site = "https://raw.githubusercontent.com/jack-mil/bing_search/master/keywords.txt?token=AOZQVABAHBKOKE4X5LVPEY262TU7O"
-word_site = "keywords.txt"
+word_file = 'keywords.txt'
+temp_file = 'tempfile'
 
 def Diff(li1, li2):
-    """ 
+    """
     Computes the difference of two lists
     """
     return list(set(li1) - set(li2))
@@ -43,9 +36,12 @@ def check_python_version():
 
 def main(argv):
     new = False
+    dryrun = False
+    count = DEFUALT_COUNT
 
+    # Arg Parse
     try:
-        opts, args = getopt.getopt(argv, 'hnc:', ['new', 'count='])
+        opts, _ = getopt.getopt(argv, 'hnc:', ['new', 'count=', 'dry-run'])
 
         for opt, arg in opts:
             if opt == '-h':
@@ -55,43 +51,50 @@ def main(argv):
                 new = True
             elif opt in ('-c', '--count'):
                 count = int(arg)
+            elif opt == '--dry-run':
+                dryrun = True
     except (ValueError, getopt.GetoptError):
-        print('Ex: python3 bing_search.py --new --count 10')
+        print('Arg parse ERROR\nEx: python3 bing_search.py --new --count 10')
         sys.exit(2)
 
     try:
+        with open(word_file, 'r') as f:
+            words = f.read().splitlines()
+            print(f'Using database of {len(words)} potential searches')
 
-        response = requests.get(word_site)
-        words = response.text.splitlines()
-
-        if not path.exists('tempfile'):
-            with open('tempfile', 'x'):
+        if not path.exists(temp_file):
+            with open(temp_file, 'x'):
                 pass
 
         if new:
             webbrowser.open_new('www.bing.com')
 
         for i in range(count):
-            with open('tempfile', 'r') as f:
+            with open(temp_file, 'r') as f:
                 used = f.read().splitlines()
 
             new = Diff(words, used)
             query = random.choice(new)
             address = url + quote_plus(query)
 
-            with open('tempfile', 'a') as f:
+            with open(temp_file, 'a') as f:
                 f.write(query + "\n")
 
-            time.sleep(sleep_time)
-            pyautogui.hotkey('alt', 'd')
-            time.sleep(0.01)
-            pyautogui.typewrite(address)
-            pyautogui.typewrite('\n', interval=0.1)
+            if not dryrun:
+                time.sleep(sleep_time)
+                pyautogui.hotkey('alt', 'd')
+                time.sleep(0.01)
+                pyautogui.typewrite(address)
+                pyautogui.typewrite('\n', interval=0.1)
 
-            print(
-                f"Search {i+1}: {query}")
+            print(f"Search {i+1}: {query}")
+
+    except FileNotFoundError:
+        print(f'File {path.realpath("word_file")} not found')
+
     finally:
-        remove('tempfile')
+        if path.exists(temp_file):
+            remove(temp_file)
 
 
 if __name__ == "__main__":
